974802514d91116ea7af519cf579ffeab7c5c8d8
Jaimi Patel <jaimip.albiorix@gmail.com>
2025-01-23 13:53:53 +0530

    fe : create main poster

---
+++ src/plugin/apis/movie.js
@@ -0,0 +1,7 @@
+const API_KEY = '63ef9c1da41cd3c843dcec9ed2264b73'
+
+const requests = {
+  getTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+}
+
+export default requests
--- src/views/MainHome.vue
+++ src/views/MainHome.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from '../components/element/NavBar.vue'
+import MainPoster from '../components/home/MainPoster.vue'
 </script>
 
 <template>
   <div>
     <Navbar />
+    <MainPoster />
   </div>
 </template>
